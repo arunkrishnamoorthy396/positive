a9fb0a748bcdd3a8a7883b34e0ed7f8c7a11ae50
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-05 09:27:46 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ public class Main{
 public static void main(String args[]){
 Scanner sc = new Scanner (System.in);
 int n = sc.nextInt();
-if(n<0)
-System.out.println("number is negative");
-else
+if(n<0){
+System.out.println("number is negative");}
+else if(n>0)
 System.out.println("number is positive");
+  else 
+    System.out.println("number is zero");
 }
 }
