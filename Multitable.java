ae5f4ee854b89e9f89323d888c887ea23622bd0e
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-10 12:29:31 +0530

    Create Multitable.java

---
+++ Multitable.java
@@ -0,0 +1,16 @@
+public class Multitable {
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int n2=1;
+	System.out.println("Enter the table number");
+	int n=sc.nextInt();
+	for(int i=1;i<=10;i++){
+		//n2=1;
+		int n1 = n2*n;
+		System.out.println(n2+"*"+n+"="+n1);
+		n2++;
+		n1++;
+	}
+	
+}
+}
