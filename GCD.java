7bd01974e0b537aaf00d24c637b3583a25f3ba28
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-11 12:30:56 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class GCD {
+	public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter two values");
+	    int n1 = sc.nextInt();
+	    int n2 = sc.nextInt();
+	    int gcd = 0;
+
+	    for (int k = 2; k <= n1 && k <= n2; k++){
+	        if(n1 % k == 0 && n2 % k == 0){
+	            gcd = k;
+	        }
+	    }
+
+	    System.out.println(gcd);
+	}
+}
