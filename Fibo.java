85eefa38158bde2f1f01980593dc45d383d7e300
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-11 18:29:46 +0530

    Create Fibo.java

---
+++ Fibo.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+ public class Fibo
+{
+public static void main(String[] args ){
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter the limit");
+	int n = sc.nextInt();
+	int sum=0;
+	int n1=0,n2=1,n3;
+	for(int i=0;i<n;i++){
+		 n3 = n1+n2;
+		 System.out.println(n3);
+		 n1=n2;
+		 n2=n3;
+ 
+	}
+}
+}
