23e4b17f9fdc30e3ca4bd97e23258e5198763f29
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-15 12:45:49 +0530

    Create Star.java

---
+++ Star.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Star {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i=n;i>0;i--){
+			for(int j=0;j<i;j++){
+			System.out.print("*");
+			
+		}
+			System.out.print(" ");
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+				
+			}
+			System.out.println();}
+		
+		
+	}
+
+}
